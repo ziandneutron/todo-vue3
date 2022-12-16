0027c38ed38bd8d352704e630de9cbf8d4a7d260
delly fauzian <fauzianddelly@gmail.com>
2022-12-16 14:32:24 +0700

    cek next page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,7 @@ import { ref, onMounted } from 'vue';
 
 const todoList = ref([]);
 const page = ref(1);
+const isDisabled = ref(false);
 
 async function getTodoList() {
   const res = await axios.get('http://localhost:3000/todo', {
@@ -14,6 +15,11 @@ async function getTodoList() {
   });
   console.log('res.data.docs', res.data)
   todoList.value = [...todoList.value, ...res.data.docs];
+  if(res.data.nextPage) {
+    isDisabled.value = false
+  } else {
+    isDisabled.value = true
+  }
 }
 
 function nextPage() {
@@ -31,5 +37,5 @@ onMounted(async () => {
   <div class="grid grid-cols-4 gap-10">
     <TodoCard v-for="(item, index) in todoList" :key="index" :index="index" :title="item.title" :description="item.description" />
   </div>
-  <button class="btn btn-primary my-5" @click="nextPage()" >Load more</button>
+  <button class="btn btn-primary my-5" @click="nextPage()" :disabled="isDisabled" >Load more</button>
 </template>
